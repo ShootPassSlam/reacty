cdf1bdf83bd625bdb3e0e92cf6b77b7460834d78
Dhruv Bhoot <dhruv@onfleet.com>
2018-11-30 17:47:33 -0800

    initial commit for Dominic

---
+++ public/app.js
@@ -0,0 +1,7 @@
+angular.module('AngularRealtime', []).controller('Main', function($scope) {
+       $scope.rows = [
+           { id: 1 },
+           { id: 2 },
+           { id: 3 }
+       ]
+});
